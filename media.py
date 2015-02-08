95b50f7496b799894c855d6ca63f2c6b0d2592f6
CruzanCaramele <letters2hamza@gmail.com>
2015-02-08 13:56:40 +0800

    Define the init function and intialize variables

--- media.py
+++ media.py
@@ -1,1 +1,8 @@
 class Movie():
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, youtube_trailer):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_img_url = poster_image
+        self.trailer_url = youtube_trailer
+        
