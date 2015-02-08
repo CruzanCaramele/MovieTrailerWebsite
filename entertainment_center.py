205c4ee72915399e56ed363ce907e0964c598c9c
CruzanCaramele <letters2hamza@gmail.com>
2015-02-08 15:36:30 +0800

    Add list of movies, titles, story lines and their trailers

---
+++ entertainment_center.py
@@ -0,0 +1,70 @@
+import media
+
+focus = media.Movie("Focus",
+                    "A story about a con man falling in love",
+                    "https://en.wikipedia.org/wiki/Focus_(2015_film)#mediaviewer/File:2015_Focus_film_poster.png",
+                    "https://www.youtube.com/watch?v=6vY9UPiI4eQ")
+
+mad_max = media.Movie("Mad Max: Fury Road",
+                      "A post-apocalyptic action film",
+                      "https://upload.wikimedia.org/wikipedia/en/f/fb/Mad_Max_Fury_Road_poster.jpg",
+                      "https://www.youtube.com/watch?v=b_4nzm9ICuo")
+
+entourage = media.Movie ("Entourage",
+                         "Four friends living the American dream in L.A",
+                         "https://upload.wikimedia.org/wikipedia/en/d/df/Entourage_film_2015_poster.jpg",
+                         "https://www.youtube.com/watch?v=Vz2HyCgUWh4")
+
+age_of_Ultron = media.Movie("Age of Ultron",
+                            "A  superhero film based on the Marvel Comics superhero team the Avengers",
+                            "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",
+                            "https://www.youtube.com/watch?v=MZoO8QVMxkk")
+
+ps_i_loveu = media.Movie("P.S I Love You",
+                     "A story about Lost Love",
+                     "http://upload.wikimedia.org/wikipedia/en/7/7f/PS_I_Love_You_%28film%29.jpg",
+                     "http://www.youtube.com/watch?v=CZzW6_hR068")
+
+about_time = media.Movie("About Time",
+                     "A story about Love and Time Travel",
+                     "http://upload.wikimedia.org/wikipedia/en/8/88/About_Time_Poster.jpg",
+                     "http://www.youtube.com/watch?v=T7A810duHvw")
+
+perks_wall_flower = media.Movie("The Perks of Being A Wall Flower",
+                     "A story about a high school kid",
+                     "http://upload.wikimedia.org/wikipedia/en/0/0b/The_Perks_of_Being_a_Wallflower_Poster.jpg",
+                     "http://www.youtube.com/watch?v=n5rh7O4IDc0")
+
+what_if = media.Movie("What If",
+                      "A  romantic comedy film about a boy miserable job and lives with his sister and nephew",
+                      "https://upload.wikimedia.org/wikipedia/en/d/d4/The_F_Word_theatrical_poster.png",
+                      "https://www.youtube.com/watch?v=A86JGbBEaBk")
+
+
+like_crazy = media.Movie("Like Crazy",
+                     "Love, Hearbreaks & Makeup",
+                     "http://upload.wikimedia.org/wikipedia/en/1/19/Like_Crazy.jpg",
+                     "http://www.youtube.com/watch?v=r-ZV-bwZmBw")
+
+terminator_genisys = media.Movie("Terminator Genisys",
+                                 "Man Against Machine",
+                                 "https://upload.wikimedia.org/wikipedia/en/f/fb/Terminator_Genisys_poster.jpg",
+                                 "https://www.youtube.com/watch?v=N4zhBQfqVCc")
+
+five_to_seven = media.Movie("5 to 7",
+                            "An aspiring novelist has an extramarital affair with a French diplomat's wife",
+                            "http://www.movie-list.com/img/posters/big/5to7.jpg",
+                            "https://www.youtube.com/watch?v=9HimzZ6QG2o")
+
+furious_seven = media.Movie("Furious 7",
+                            "Sequel to the 2013 film Fast & Furious 6 and the seventh installment in the Fast & Furious film series.",
+                            "https://upload.wikimedia.org/wikipedia/en/b/b8/Furious_7_poster.jpg",
+                            "https://www.youtube.com/watch?v=yISKeT6sDOg")
+
+
+                      
+movies = [focus, mad_max, entourage, age_of_Ultron, ps_i_loveu, about_time,
+          perks_wall_flower, what_if, like_crazy, terminator_genisys, five_to_seven,
+          furious_seven]
+
+
