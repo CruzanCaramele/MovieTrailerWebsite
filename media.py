adcbe5364fb631073273279d67ca7f2d7c9eaea1
CruzanCaramele <letters2hamza@gmail.com>
2015-02-08 18:29:31 +0800

    Add docs for movie class

--- media.py
+++ media.py
@@ -5,15 +5,19 @@ initialized."""
 import webbrowser
 
 class Movie():
-    #constructor, creates a movie object
+    #A class theat defines a Movie
+    
     def __init__(self, movie_title, movie_storyline,
                  poster_image, youtube_trailer):
+        #constructor, creates a movie object
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = youtube_trailer
 
-    """ A function that takes in an instance of the Movie class
-    and opens it's trailer in the web browser"""
+
     def show_trailer(self):
+        """ A function that takes in an instance of the Movie class
+        and opens it's trailer in the web browser
+        """
         webbrowser.open(self.trailer_url)
