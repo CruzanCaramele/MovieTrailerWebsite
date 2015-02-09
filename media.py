05c632e6bf3daaee38cb5e74b677ee4d8c157730
CruzanCaramele <letters2hamza@gmail.com>
2015-02-09 18:47:05 +0800

    Add variables for the 2 new attributes release dates & genre

--- media.py
+++ media.py
@@ -8,12 +8,15 @@ class Movie():
     #A class theat defines a Movie
     
     def __init__(self, movie_title, movie_storyline,
-                 poster_image, youtube_trailer):
+                 poster_image, youtube_trailer,
+                 release_date, genre):
         #constructor, creates a movie object
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = youtube_trailer
+        self.release_date = release_date
+        self.genre = genre
 
 
     def show_trailer(self):
