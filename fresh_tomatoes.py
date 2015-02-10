dbf5c975dc4555d82aac45eeab0bf853b8cc9972
CruzanCaramele <letters2hamza@gmail.com>
2015-02-10 16:19:59 +0800

    Move facebook script to head

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -79,6 +79,15 @@ main_page_head = '''
           });
         });
     </script>
+        <!-- Facebook share -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 </head>
 '''
 
@@ -88,15 +97,7 @@ main_page_content = '''
 <html lang="en">
   <body>
   
-    <!-- Facebook share -->
-    <div id="fb-root"></div>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+
         
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
